d7b8b2625bb055310a2a4fd38d618929f9a47f3f
Mateus Ariel Pauli <62779446+MateusArielPauli@users.noreply.github.com>
2023-09-15 14:07:25 -0300

    tarefa3

---
+++ src/main/java/br/edu/utfpr/commerceapi/dto/AuthDTO.java
@@ -0,0 +1,21 @@
+package br.edu.utfpr.commerceapi.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+public class AuthDTO {
+    @NotBlank
+    @Size(min = 3)
+    public String username;
+
+    @NotBlank
+    @Size(min = 3)
+    public String password;
+}
